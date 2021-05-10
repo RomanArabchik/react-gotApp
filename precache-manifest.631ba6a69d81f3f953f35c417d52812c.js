0580befb72662614bb1dcb9207418ad040a27460
Roman Arabchik <roma_x24@mail.ru>
2021-05-10 18:36:13 +0300

    Updates

---
+++ precache-manifest.631ba6a69d81f3f953f35c417d52812c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a584cb1eb8ececa9784",
+    "url": "/react-gotApp/static/css/main.99617aa0.chunk.css"
+  },
+  {
+    "revision": "1a584cb1eb8ececa9784",
+    "url": "/react-gotApp/static/js/main.f066d769.chunk.js"
+  },
+  {
+    "revision": "854a68c27a13ce8a8e4e",
+    "url": "/react-gotApp/static/js/runtime~main.6da7bfe7.js"
+  },
+  {
+    "revision": "796142a184a9bd8e4072",
+    "url": "/react-gotApp/static/css/2.5a6f744c.chunk.css"
+  },
+  {
+    "revision": "796142a184a9bd8e4072",
+    "url": "/react-gotApp/static/js/2.fc253e3a.chunk.js"
+  },
+  {
+    "revision": "df6fc9510291e725e7eea4a6620e005b",
+    "url": "/react-gotApp/static/media/error.df6fc951.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/react-gotApp/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "c49d57f871d79efb9d85fa3dcb5cb16c",
+    "url": "/react-gotApp/index.html"
+  }
+];
